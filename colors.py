f8aa9f4f6f49e39cb880acf5a777a151ae936561
JohnathanPi <88287255+JohnathanPi@users.noreply.github.com>
2021-10-06 23:14:09 +0300

    delete colors,py

--- colors.py
+++
@@ -1,8 +0,0 @@
-def color_array_template():
-    return ["#" for i in range(15)]
-
-nice_colors = color_array_template()
-print(nice_colors)
-
-['#264653', '#023e8a', '#7f5539', '#3a0ca3', '#6d6875', '#005f73', '#370617', 
-'#212529', '#495057', '#118ab2', '#34a0a4', '#ee6c4d', '#9a031e', '#3c096c', '#3c096c']
